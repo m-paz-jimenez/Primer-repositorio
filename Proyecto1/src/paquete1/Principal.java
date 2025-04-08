8763af4b0c9340ff37ac3756b25f8480c3a3cd3f
M.Paz Jimenez Martin <mjimmar648@educaand.es>
2025-04-08 13:30:03 +0200

    Segundo commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo un control de versiones");
+		System.out.println("Realizamos cambios");
 	}
 }
