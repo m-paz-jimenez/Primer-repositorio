53275778244a1916a880830381e5fff01cf5d445
M.Paz Jimenez Martin <mjimmar648@educaand.es>
2025-04-21 14:11:54 +0200

    "primer commit rama1"

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -6,5 +6,7 @@ public class Principal {
 		System.out.println("Estamos haciendo un control de versiones");
 		System.out.println("Realizamos cambios");
 		System.out.println("Realizamos un segundo cambio");
+		System.out.println("Creamos rama1");
+		System.out.println("Realizamos cambios");
 	}
 }
